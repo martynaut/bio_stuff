2f47325489ccfc95280284c60152535e99153d11
science-is-the-new-black <scienceisthenewblack@gmail.com>
2017-04-15 07:02:05 +0200

    mail sending

--- science.py
+++ science.py
@@ -2,6 +2,37 @@ import cherrypy
 import uuid
 from reverse_complement.revcom import rev_comp_change
 from cherrypy.lib.static import serve_fileobj
+from email.mime.multipart import MIMEMultipart
+from email.utils import formatdate
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email import encoders
+import smtplib
+
+
+def send_email_results(email, files):
+
+    msg = MIMEMultipart()
+    msg['From'] = 'science <science@scienceisthenewblack.eu>'
+    msg['To'] = 'scienceisthenewblack_user <'+email+'>'
+    msg['Date'] = formatdate(localtime=True)
+    msg['Subject'] = 'Results from scienceisthenewblack'
+    msg.attach(MIMEText('in the attachment file with the results\n\n' +
+                        'scienceisthenewblack team'))
+    for file in files:
+        with open(file) as file_read:
+            part = MIMEBase('application', "octet-stream")
+            part.set_payload(file_read.read().replace('<br />', '\n'))
+            encoders.encode_base64(part)
+            part.add_header('Content-Disposition',
+                            'attachment; filename="'+file+'"')
+            msg.attach(part)
+    smtp = smtplib.SMTP("localhost")
+    smtp.sendmail(
+                  'mirnamotif <mirnamotif@scienceisthenewblack.eu>',
+                  ['mirnamotif_user <'+email+'>'],
+                  msg.as_string())
+    smtp.close()
 
 
 class Page:
